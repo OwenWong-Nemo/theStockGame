5732dd58978f99c96c9281baa1af9859689a0381
Ho Hang Owen Wong <owon0011@student.monash.edu>
2026-01-09 14:44:10 +1100

    Testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+    std::string companies10;
+    std::ifstream file("assets/companies10.txt");
+    if (file.is_open()) {
+        while (getline(file, companies10)) {
+            std::cout << companies10 << std::endl;
+        }
+        file.close();
+    } else {
+        std::cout << "Unable to open file";
+    }
+
+    std::cout << companies10 << std::endl;
 }
